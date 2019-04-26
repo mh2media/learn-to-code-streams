14efb8b343138cf71a710a915bf98e5173037973
m.henry@mh2media.com <m.henry@mh2media.com>
2019-04-26 15:41:16 -0700

    Lesson 2 Prime Numbers

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      for (int potentialPrime = 2; potentialPrime < 100; potentialPrime++)
+      {
+        if (IsPrime(potentialPrime))
+        {
+          Console.Write(potentialPrime);
+          Console.Write(", ");
+        }
+
+      }
+    }
+
+    private static bool IsPrime(int potentialPrime)
+    {
+        int maxDivisor = (int)Math.Sqrt(potentialPrime);
+      for (int divisor = 2; divisor <= maxDivisor; divisor++)
+      {
+          if (potentialPrime % divisor == 0)
+          {
+           return false;   
+          }
+      }
+
+          return true;
+    }
+  }
+}
